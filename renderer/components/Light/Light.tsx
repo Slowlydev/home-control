1664d8a3a04ed89780f74053fae88d01fa1c77a3
Slowlydev <slowly.developers@gmail.com>
2022-04-14 15:45:14 +0200

    updated: switch to threejs less component

--- renderer/components/Light/Light.tsx
+++ renderer/components/Light/Light.tsx
@@ -1,21 +1,18 @@
-import { Canvas } from '@react-three/fiber'
-import { EffectComposer, Bloom } from '@react-three/postprocessing'
+import styles from "./Light.module.scss";
 
-export default function Light({ color, on }: { color: string, on: boolean }) {
+interface LightProps {
+	isOn: boolean,
+	color: string,
+}
 
+export default function Light({ isOn, color }: LightProps) {
 	return (
-		<Canvas style={{ height: 100, width: 100 }} dpr={[1, 1.5]} camera={{ position: [0, 0, 15] }}>
-			<pointLight position={[15, 15, 15]} color={!on ? '#ffffff' : color} />
+		<div className={styles.lightContainer}>
+			<div className={styles.light} style={{ backgroundColor: isOn ? color : "black" }} />
 
-			<mesh {...{ scale: 0.8 }}>
-				<sphereBufferGeometry args={[5, 24, 24]} />
-				<meshBasicMaterial color={!on ? '#000000' : color} toneMapped={false} />
-			</mesh >
-
-			<EffectComposer multisampling={8}>
-				<Bloom kernelSize={3} luminanceThreshold={0} luminanceSmoothing={0.3} intensity={0.5} />
-				<Bloom kernelSize={2} luminanceThreshold={0} luminanceSmoothing={0} intensity={0.4} />
-			</EffectComposer>
-		</Canvas>
+			<div className={styles.background}>
+				<div className={styles.light} style={{ backgroundColor: isOn ? color : "black" }} />
+			</div>
+		</div>
 	)
 }
