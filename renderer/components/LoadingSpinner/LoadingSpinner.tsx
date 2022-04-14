573a0d3d7d3a50ff189530d7824245b7d4bafc66
Slowlydev <slowly.developers@gmail.com>
2022-04-14 15:45:45 +0200

    added: loading spinner component

---
+++ renderer/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,12 @@
+import { motion } from "framer-motion"
+
+import styles from "./LoadingSpinner.module.scss";
+
+export default function LoadingSpinner() {
+	return (
+		<motion.div className={styles.wrapper}>
+			<motion.div className={styles.dot} animate={{ opacity: [0.2, 1, 0.2], scale: [0.5, 1, 0.5], transition: { repeat: Infinity } }} />
+			<motion.div className={styles.dot} animate={{ opacity: [1, 0.2, 1], scale: [1, 0.5, 1], transition: { repeat: Infinity } }} />
+		</motion.div>
+	)
+}
