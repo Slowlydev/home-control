08d672ba99c27ccd1d7c93f76af5ad4564d9cfdb
Slowlydev <slowly.developers@gmail.com>
2022-04-25 21:35:24 +0200

    updated: bridge info service command

--- renderer/services/bridge.service.ts
+++ renderer/services/bridge.service.ts
@@ -2,16 +2,17 @@ import Store from "electron-store";
 import axios from "axios";
 
 import ClinetKeyInterface from "../interfaces/ClientKeyInterface";
+import BridgeConfigInterface from "../interfaces/BridgeConfigInterface";
 
 const store = new Store();
 
-async function getBridgeInfo(): Promise<any> {
-	const bridge = store.get("bridge");
-	if (bridge) {
-		return axios.get(`http://${bridge}/clip/v2/resource/device`).then(res => res.data);
-	} else {
-		throw new Error("Could not get bridge info");
-	}
+const httpRequest = axios.create({
+	baseURL: `http://${store.get('bridge')}/api/${store.get('token')}/`
+});
+
+async function getBridgeInfo(): Promise<BridgeConfigInterface> {
+	const { data } = await httpRequest.get("/config");
+	return data;
 }
 
 async function createClientKey(): Promise<{ success: ClinetKeyInterface }[]> {
