5be6bdd776c8f97bda0fbba3255f7eed9e7a3015
Slowlydev <slowly.developers@gmail.com>
2022-04-25 21:33:48 +0200

    added: functionality to birdge status component

--- renderer/components/BridgeStatus/BridgeStatus.tsx
+++ renderer/components/BridgeStatus/BridgeStatus.tsx
@@ -1,24 +1,32 @@
+import useSWR from "swr";
+
 import styles from "./BridgeStatus.module.scss";
+
+import bridgeService from "../../services/bridge.service";
 
 export default function BridgeStatus() {
 
-	// todo: logic 
+	const { data, isValidating } = useSWR("bridgeInfo", bridgeService.getBridgeInfo);
+
+	const hasNoUpdates = data?.swupdate2.state === "noupdates";
+	const hasInternet = data?.internetservices.internet === "connected";
+	const hasConnectivity = data !== undefined;
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.wrapper}>
-				<div className={styles.dot} data-isoffline />
+				<div className={`${styles.dot} ${!hasNoUpdates ? styles.error : void 0}`} />
+				<p>Updates</p>
+			</div>
+
+			<div className={styles.wrapper}>
+				<div className={`${styles.dot} ${!hasInternet ? styles.error : void 0}`} />
+				<p>Internet</p>
+			</div>
+
+			<div className={styles.wrapper}>
+				<div className={`${styles.dot} ${!hasConnectivity ? styles.error : void 0}`} />
 				<p>Connectivity</p>
-			</div>
-
-			<div className={styles.wrapper}>
-				<div className={styles.dot} />
-				<p>Authentication</p>
-			</div>
-
-			<div className={styles.wrapper}>
-				<div className={styles.dot} />
-				<p>Discovery</p>
 			</div>
 		</div>
 	)
