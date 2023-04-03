47e7d24316be182cf813be2209b0f5a89243204c
Slowlydev <slowly.developers@gmail.com>
2023-04-03 19:32:20 +0200

    feat: setup vite with swc

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), tsconfigPaths()],
+	// prevent vite from obscuring rust errors
+	clearScreen: false,
+	// Tauri expects a fixed port, fail if that port is not available
+	server: {
+		strictPort: true,
+	},
+	// to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
+	// `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
+	// env variables
+	envPrefix: ["VITE_", "TAURI_"],
+	build: {
+		target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+		minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+		sourcemap: !!process.env.TAURI_DEBUG,
+	},
+});
