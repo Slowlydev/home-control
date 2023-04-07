8d7dbd294e562be66d9b712a32a7bd8781b1a4b3
Slowlydev <slowly.developers@gmail.com>
2023-04-07 17:08:44 +0200

    refactor(services): rename everything to cloud because it sounds cooler

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -1,9 +1,9 @@
 import { Token } from "@/types/Token.type";
 
-import { remoteToken } from "./axios.service";
+import { cloudToken } from "./axios.service";
 
 export const requestToken = async (code: string): Promise<Token> => {
-	const { data } = await remoteToken.post("", null, {
+	const { data } = await cloudToken.post("", null, {
 		headers: {
 			grant_type: `authorization_code&code=${code}`,
 		},
@@ -12,7 +12,7 @@ export const requestToken = async (code: string): Promise<Token> => {
 };
 
 export const refreshToken = async ({ refreshToken }: { refreshToken: string }): Promise<Token> => {
-	const { data } = await remoteToken.post("", null, {
+	const { data } = await cloudToken.post("", null, {
 		headers: {
 			grant_type: `refresh_token&refresh_token=${refreshToken}`,
 		},
--- src/services/axios.service.ts
+++ src/services/axios.service.ts
@@ -4,18 +4,18 @@ import axios from "axios";
 import { env } from "@/lib/config";
 
 type Store = {
-	strategy: "local" | "remote" | "dynamic" | null;
+	strategy: "local" | "cloud" | "dynamic" | null;
 	local: {
 		bridge: string;
 		key: string;
 	} | null;
-	remote: {
+	cloud: {
 		key: string;
 	} | null;
 	token: Token | null;
 };
 
-const store: Store = { strategy: null, local: null, remote: null, token: null };
+const store: Store = { strategy: null, local: null, cloud: null, token: null };
 
 export const localBridge = axios.create({
 	baseURL: `https://${store.local?.bridge}/clip/v2/`,
@@ -24,15 +24,15 @@ export const localBridge = axios.create({
 	},
 });
 
-export const remoteBridge = axios.create({
+export const cloudBridge = axios.create({
 	baseURL: `https://api.meethue.com/route/clip/v2/`,
 	headers: {
 		Authorization: `Bearer ${store.token?.access_token}`,
-		"hue-application-key": store.remote?.key,
+		"hue-application-key": store.cloud?.key,
 	},
 });
 
-export const remoteToken = axios.create({
+export const cloudToken = axios.create({
 	url: "https://api.meethue.com/v2/oauth2/token",
 	headers: {
 		Authorization: `Basic ${btoa(`${env.VITE_APP_CLIENT_ID}:${env.VITE_APP_CLIENT_SECRET}`)}`,
