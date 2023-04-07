5d67f5adcb0a4a6a812e8bf8e93baf55a3fb2d21
Slowlydev <slowly.developers@gmail.com>
2023-04-07 16:58:56 +0200

    feat(app-router): added router to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
+import SetupPage from "./pages/setup";
+
 export default function App() {
-	return <div></div>;
+	return (
+		<div>
+			<Routes>
+				<Route path="/" element={<SetupPage />} />
+			</Routes>
+		</div>
+	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import "./styles/globals.scss";
@@ -7,11 +8,13 @@ import "./styles/globals.scss";
 const element = document.getElementById("root");
 
 if (!element) {
-	throw new Error("element with id app was not found");
+	throw new Error("element with id root was not found");
 }
 
 createRoot(element).render(
 	<StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</StrictMode>,
 );
