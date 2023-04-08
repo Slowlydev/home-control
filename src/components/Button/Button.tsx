64c3f85af0c0993d019c0b42fa024db1e1e22199
Slowlydev <slowly.developers@gmail.com>
2023-04-08 13:51:19 +0200

    feat(button): add disabled styles and functionality

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,26 @@
+import classNames from "classnames";
 import { motion } from "framer-motion";
 import { MouseEventHandler, ReactNode } from "react";
+
+import styles from "./Button.module.scss";
 
 type Props = {
 	onClick?: MouseEventHandler;
 	children?: ReactNode;
+	className?: string;
+	disabled?: boolean;
 };
 
-export default function Button({ onClick, children }: Props) {
+export default function Button({ onClick, children, className, disabled }: Props) {
 	return (
-		<motion.button onClick={onClick} initial={{ scale: 1.0 }} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+		<motion.button
+			disabled={disabled}
+			onClick={(e) => (!disabled && onClick ? onClick(e) : void 0)}
+			className={classNames(className, styles.button)}
+			initial={{ scale: 1.0 }}
+			whileHover={{ scale: 1.1 }}
+			whileTap={{ scale: 0.9 }}
+		>
 			{children}
 		</motion.button>
 	);
